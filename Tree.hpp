e2771f2dbda34f4cb0f98c0ec9e6097942da8dbc
FerasTr <daragma.feras@gmail.com>
2019-03-15 18:05:01 +0200

    hpp

--- Tree.hpp
+++ Tree.hpp
@@ -13,3 +13,25 @@ struct node
     node *left;
     node *right;
 };
+
+class Tree
+{
+  private:
+    node *rootNode;
+    int numOfNodes;
+
+    node *search(int num);
+    node *SearchHelper(int num, node *curr);
+
+  public:
+    bool insert(int i);
+    bool remove(int num);
+    int size();
+    bool contains(int num);
+    int root();
+    int parent(int num);
+    int left(int num);
+    int right(int num);
+    void print();
+    void inOrder(node *root);
+};
